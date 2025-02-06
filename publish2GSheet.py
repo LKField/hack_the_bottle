223fc97b82239bb70db7ef7c9b2542d534667c7f
lucretiafield <lucretia.k.field@gmail.com>
2025-02-06 12:33:13 +0100

    create gitignore and recommit

--- publish2GSheet.py
+++ publish2GSheet.py
@@ -1,3 +1,7 @@
+# Code Source: http://www.whatimade.today/log-sensor-data-straight-to-google-sheets-from-a-raspberry-pi-zero-all-the-python-code/
+# Original Author: Allan Schwartz
+# Modified by: Lucretia Field 
+
 # import many libraries
 from __future__ import print_function  
 from googleapiclient.discovery import build  
@@ -9,15 +13,14 @@ import datetime
 # My Spreadsheet ID ... See google documentation on how to derive this
 MY_SPREADSHEET_ID = '1y0pz5SzXEUc4PvjJy_hvA48g36e0usQjLvp9BQxHJxs'
 
-temperature = 10
-pressure = 70 
-humidity = 30
+# The ID for our specific device - used for if we have multiple devices 
+DEVICE_ID = 0
 
 
-def update_sheet(sheetname="Sheet1", temperature, pressure, humidity):  
+def update_sheet(sheetname="Sheet1", link="0", title="0", channel="0"):  
     """update_sheet method:
        appends a row of a sheet in the spreadsheet with the 
-       the latest temperature, pressure and humidity sensor data
+       the link, title, and channel information 
     """
     # authentication, authorization step
     SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
@@ -27,13 +30,14 @@ def update_sheet(sheetname="Sheet1", temperature, pressure, humidity):
 
     # Call the Sheets API, append the next row of sensor data
     # values is the array of rows we are updating, its a single row
-    values = [ [ str(datetime.datetime.now()), 
-        'Temperature', temperature, 'Pressure', pressure, 'Humidity', humidity ] ]
+    # Format: Datetime	Device ID	Video Link	Video Name	Channel Name
+    values = [ [ str(datetime.datetime.now()), DEVICE_ID,
+                 link, title, channel ] ]
     body = { 'values': values }
     # call the append API to perform the operation
     result = service.spreadsheets().values().append(
                 spreadsheetId=MY_SPREADSHEET_ID, 
-                range=sheetname + '!A1:G1', 
+                range=sheetname + '!A2:E2', 
                 valueInputOption='USER_ENTERED', 
                 insertDataOption='INSERT_ROWS',
                 body=body).execute()                     
@@ -44,10 +48,14 @@ def main():
        reads the BME280 chip to read the three sensors, then
        call update_sheets method to add that sensor data to the spreadsheet
     """
-    print ('Temperature: %f °C' % temperature)
-    print ('Pressure: %f hPa' % pressure)
-    print ('Humidity: %f %%rH' % humidity)
-    update_sheet("Sheet1", temperature, pressure, humidity)
+    link = "https://www.youtube.com/watch?v=VD6xJq8NguY"
+    title = "There Is Something Hiding Inside Earth"
+    channel = "Kurzgesagt - In a Nutshell"
+
+    print ('Link: ', link)
+    print ('Title: ', title)
+    print ('Channel: ', channel)
+    update_sheet("Sheet1", link, title, channel)
 
 
 if __name__ == '__main__':  
